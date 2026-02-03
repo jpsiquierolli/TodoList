0ec21ec830d09b63b428d4879dda5514575ebce0
feliperscunha <ruascunhafelipe@gmail.com>
2026-02-03 18:23:04 -0300

    Primeira parte do AuthViewModel

--- app/src/main/java/com/example/todolist/ui/feature/pages/AuthViewModel.kt
+++ app/src/main/java/com/example/todolist/ui/feature/pages/AuthViewModel.kt
@@ -1,6 +1,49 @@
 package com.example.todolist.ui.feature.pages
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseAuth
 
 class AuthViewModel : ViewModel() {
+
+    private val auth : FireBaseAuth = FireBaseAuth.getInstance()
+    private val _authState = MutableLiveData<AuthState>()
+    val authState: LiveData<AuthState> = _authState
+
+    init{
+        checkAuthStatus()
+    }
+
+    fun checkAuthStatus() {
+        if(auth.currentUser == null){
+            _authState.value = AuthState.Unauthenticated
+        } else {
+            _authState.value = AuthState.Authenticated
+        }
+    }
+
+    fun login (email : String, password : String) {
+
+        if (email.isEmpty() || password.isEmpty()) {
+            _authState.value = AuthState.Error("Email and password cannot be empty")
+            return
+        }
+        _authState.value = AuthState.Loading
+        auth.SignInWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    _authState.value = AuthState.Authenticated
+                } else {
+                    _authState.value = AuthState.Error(task.exception?.message ?: "Unknown error")
+                }
+    }
+}
+
+sealed class AuthState {
+    object Authenticated : AuthState()
+    object Unauthenticated : AuthState()
+    object Loading : AuthState()
+    data class Error(val message: String) : AuthState()
+
 }
